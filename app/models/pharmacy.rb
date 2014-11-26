1439ad3dc713dbd73ac4e0d3b5b58b7907a72135
yiannis <yiannispol@gmail.com>
2014-11-26 14:27:52 +0200

    Application Style

--- app/models/pharmacy.rb
+++ app/models/pharmacy.rb
@@ -1,11 +1,18 @@
 class Pharmacy < ActiveRecord::Base
 	belongs_to :city
-	geocoded_by :address
+	geocoded_by :my_address
 	after_validation :geocode #do the geocoding after validation
+
+
+	#geocode with address and location
+	def my_address
+		"#{address}, #{location}"
+	end
+
 
 	def self.import(file)
 		CSV.foreach(file.path , encoding: "iso-8859-7:UTF-8", headers: true) do |row| #iso-8859-7 is for the greek lanugage encoding	
-			Pharmacy.create! row.to_hash
+			Pharmacy.create! row.to_hash #write the records in database
 		end
 	end
 end		
